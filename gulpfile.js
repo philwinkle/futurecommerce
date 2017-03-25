080be62030c9ee1706f0d567ecd0aa6127fefa6c
Nadav Spiegelman <me@nadav.name>
2017-03-25 13:13:15 -0400

    Add stylelint

--- gulpfile.js
+++ gulpfile.js
@@ -14,11 +14,28 @@ gulp.task('serve', ['sass'], function() {
   gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
+gulp.task('lint-css', function lintCssTask() {
+  const gulpStylelint = require('gulp-stylelint');
+
+  return gulp
+    .src('src/scss/*.scss')
+    .pipe(gulpStylelint({
+      reporters: [
+        { formatter: 'string', console: true }
+      ],
+      failAfterError: false,
+    }));
+});
+
 // Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
+gulp.task('sass', ['lint-css'], function() {
   return gulp.src("src/scss/*.scss")
     .pipe(sass().on('error', sass.logError))
-    .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }), csswring() ]))
+    .pipe(postcss([ 
+      autoprefixer({ browsers: ['last 2 versions'] }),
+      csswring(),
+      require('postcss-browser-reporter'),
+    ]))
     .pipe(gulp.dest("src/css"))
     .pipe(browserSync.stream());
 });
